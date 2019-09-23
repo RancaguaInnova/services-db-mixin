2476bbd08163782f674607e16e3c8f37a5b5b9d1
Joaquín Gumucio <jjgumucio@gmail.com>
2019-09-23 12:30:17 -0300

    refactoring

--- src/loginfo.mixin.js
+++ src/loginfo.mixin.js
@@ -1,6 +1,40 @@
 const capitalize = require('lodash/capitalize')
 const includes = require('lodash/includes')
 /**
+ * Logs Event
+ *
+ * @param {Object} logger Logger object
+ * @param {String} logger.version Version of the logger
+ * @param {String} logger.name Resource/endpoint/table
+ * @param {String} logger.action Action performed
+ * @param {String} origin Origin header. Where this request was made from
+ * @param {String} resource Resource/endpoint/table
+ * @param {String} action Action performed
+ * @param {String} userId Id of the logged in user who performed the action
+ * @param {String} entityId Id of the document created or modified
+ */
+const logEvent = function(logger, origin, resource, action, userId, entityId) {
+  const logDoc = {
+    origin: origin || '',
+    date: new Date(),
+    resource: capitalize(resource),
+    action,
+    entityId,
+    userId: userId
+  }
+
+  this.logger.info(logDoc)
+  if (logger) {
+    if (resource !== logger.name) {
+      context.call(
+        `v${logger.version}.${logger.name}.${logger.action}`,
+        logDoc
+      )
+    }
+  }
+}
+
+/**
  * Logs DB operations to broker logger or a personalized logger service
  *
  * @param {Object?} logger
@@ -12,7 +46,6 @@ const includes = require('lodash/includes')
 module.exports = function (logger) {
   return {
     methods: {
-
       /**
      * Log operations to passed logger
      *
@@ -27,23 +60,7 @@ module.exports = function (logger) {
         const userId = user && user.id ? user.id : ''
 
         if (includes(['create', 'update', 'remove'], action)) {
-          const logDoc = {
-            origin: origin || '',
-            date: new Date(),
-            resource: capitalize(resource),
-            action,
-            entityId,
-            userId: userId
-          }
-          this.logger.info(logDoc)
-          if (logger) {
-            if (resource !== logger.name) {
-              context.call(
-                `v${logger.version}.${logger.name}.${logger.action}`,
-                logDoc
-              )
-            }
-          }
+          logEvent(logger, origin, resource, action, userId, entityId)
         }
       }
     }
